cf430ed7eb17c6128c3fde6b6442dd983e825855
Simona Holubkova <2483856H@student.gla.ac.uk>
2021-01-25 01:14:21 +0100

    chapter 7 w/o exercises

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rango.models import Category, Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -36,6 +39,21 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango')
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
 
 
 
